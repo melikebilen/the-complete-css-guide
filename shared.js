8999af11fa944705b391b90a6225f67f470c7734
Melike Bilen <bilenmelike@gmail.com>
2025-08-23 02:32:34 +0300

    lesson 135

--- shared.js
+++ shared.js
@@ -1,5 +1,10 @@
 var backdrop = document.querySelector('.backdrop');
-//console.log(backdrop);
-//console.dir(backdrop);
+var modal = document.querySelector('.modal');
+var selectPlanButtons = document.querySelectorAll('.plan button');
 
-backdrop.style.display = 'block';
+for (var i = 0; i < selectPlanButtons.length; i++) {
+    selectPlanButtons[i].addEventListener('click', function() {
+        modal.style.display = 'block';
+        backdrop.style.display = 'block';
+    });
+}
