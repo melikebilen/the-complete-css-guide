17d49ca39ab7d3e3aed0ce0e6ca9f7a7e4fa7024
Melike Bilen <bilenmelike@gmail.com>
2025-08-25 18:45:13 +0300

    including the closing button

--- shared.js
+++ shared.js
@@ -1,10 +1,21 @@
 var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
 var selectPlanButtons = document.querySelectorAll('.plan button');
+var modalNoButton = document.querySelector('.modal__action--negative');
 
+// console.dir(backdrop);
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function() {
         modal.style.display = 'block';
         backdrop.style.display = 'block';
     });
+}   
+
+modalNoButton.addEventListener('click', closeModal);
+
+backdrop.addEventListener('click', closeModal);
+
+function closeModal() {
+    modal.style.display = 'none';
+    backdrop.style.display = 'none';
 }
