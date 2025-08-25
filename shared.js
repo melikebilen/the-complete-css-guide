e951057130a876c2490ea02afed6459f58e64d15
Melike Bilen <bilenmelike@gmail.com>
2025-08-25 19:35:26 +0300

    lesson 138 - manipulating element classes

--- shared.js
+++ shared.js
@@ -8,24 +8,24 @@ var mobileNav = document.querySelector('.mobile-nav');
 // console.dir(backdrop);
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function() {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
+        modal.classList.add('open');
+        backdrop.classList.add('open');
     });
 }   
 
 modalNoButton.addEventListener('click', closeModal);
 
 backdrop.addEventListener('click', function () {
-    mobileNav.style.display = "none";
+    mobileNav.classList.remove('open');
     closeModal();
 });
 
 function closeModal() {
-    modal.style.display = 'none';
-    backdrop.style.display = 'none';
+    modal.classList.remove('open');
+    backdrop.classList.remove('open');
 }
 
 toggleButton.addEventListener('click', function (){
-    mobileNav.style.display = "block";
-    backdrop.style.display = "block";
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
 });
